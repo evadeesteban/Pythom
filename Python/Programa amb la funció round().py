33542624b2010d9229872e0421882fa5cd074d0b
evadeesteban <71399440+evadeesteban@users.noreply.github.com>
2021-01-20 19:45:44 +0100

    Add files via upload

---
+++ Python/Programa amb la funció round().py
@@ -0,0 +1,9 @@
+nota1 = float(input("Introdueix la teva nota "))
+final1 = round(nota1)
+print("El arrodoniment de la nota és:{}".format (final1))
+nota2 = float(input("Introdueix la teva nota d'anglès "))
+final2 = round(nota2)
+print("El arrodoniment de la nota és:{}".format (final2))
+nota3 = nota1+nota2
+final3 = round(nota3)
+print("Nota final:{}".format (final3))
